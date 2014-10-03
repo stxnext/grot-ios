2e2b21a7f31026c0589f138d54f84e9d502d3615
Dawid Żakowski <dawid.zakowski@stxnext.pl>
2014-10-03 12:13:27 +0200

    Initial Commit

---
+++ Grot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grot
+//
+//  Created by Dawid Żakowski on 03/10/2014.
+//  Copyright (c) 2014 STX Next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Grot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Grot
+//
+//  Created by Dawid Żakowski on 03/10/2014.
+//  Copyright (c) 2014 STX Next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
